76c90ef0e785ce28e88a78d9f228be7bb0ad66ec
omor shahriar <omor.shahriar2000@gmail.com>
2022-01-01 15:13:58 +0600

    theme color test

--- components/MessengerChatHead.js
+++ components/MessengerChatHead.js
@@ -2,7 +2,11 @@ import MessengerCustomerChat from "react-messenger-customer-chat";
 
 const MessengerChatHead = () => {
   return (
-    <MessengerCustomerChat pageId="100213708956649" appId="953995268541996" />
+    <MessengerCustomerChat
+      pageId="100213708956649"
+      appId="953995268541996"
+      themeColor="#3CBB95"
+    />
   );
 };
 
